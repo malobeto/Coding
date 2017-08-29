1ac5697697dbac705cd228aad7b934ee6d539740
malobeto <31299937+malobeto@users.noreply.github.com>
2017-08-28 19:59:59 -0500

    Add files via upload

---
+++ inventory.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+  const int MAX_ITEMS = 10;
+  string inventory[Max_ITEMS];
+
+  int numItems = 0;
+  invetory[numItems++] = "sword";
+  invetory[numItems++] = "armor";
+  invetory[numItems++] = "shield";
+
+   cout << "Your items:\n";
+   for(int i=0; i < numItems; i++)
+   {
+     cout << inventory[i] << endl;
+   }
+  }
+  return 0;
+}
